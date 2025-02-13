2787bf484217bf21e64f6f20a7279e98615fa05e
u1and0 <e01.ando60@gmail.com>
2025-02-13 18:54:42 +0900

    [doc] add file path option for help message and README

--- lib/help.ts
+++ lib/help.ts
@@ -24,6 +24,7 @@ export const helpMessage =
       -t, --temperature: number Higher number means more creative answers, lower number means more exact answers (default 1.0)
       -u, --url: string URL and port number for ollama server
       -s, --system-prompt: string The first instruction given to guide the AI model's response
+      -f, --file: string  Attachment file path.
       -n, --no-chat: boolean   No chat mode. Just one time question and answer.
     PROMPT:
       string A Questions for Model
