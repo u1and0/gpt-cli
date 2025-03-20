5f5ec75708a9014bd7e57c06f54831c55c75e84e
u1and0 <e01.ando60@gmail.com>
2025-03-20 14:02:22 +0900

    [doc] モデルの追加の仕方

--- lib/model.ts
+++ lib/model.ts
@@ -1,6 +1,28 @@
 /**
  * 特定企業に依存したクローズドモデルのLLMインスタンスを生成する関数を定義します。
  * その呼出元modelMapを公開します。
+ *
+ * # モデルの追加の仕方
+ *
+ * 1.  対応するLangChainのモデルのimportを追加します。
+ *     例: `import { ChatHogeHoge } from "npm:@langchain/hoge-hoge";`
+ * 2.  `CloseModel` 型に、追加したモデルの型を追加します。
+ *     例: `| ChatHogeHoge;`
+ * 3.  モデルのインスタンスを生成する関数を作成します。
+ *     - Paramsを受け取り、LangChainのモデルを返す関数
+ *     例:
+ *     ```typescript
+ *     const createHogeHogeInstance = (params: Params): ChatHogeHoge => {
+ *       return new ChatHogeHoge({
+ *         modelName: params.model,
+ *         temperature: params.temperature,
+ *         maxTokens: params.maxTokens,
+ *       });
+ *     };
+ *     ```
+ * 4.  `modelMap` に、モデルを識別するためのキーと、3で作成した関数を登録します。
+ *     - キーは、モデル名にマッチする正規表現です。
+ *     例: `"^hoge": createHogeHogeInstance,`
  */
 import { ChatOpenAI } from "npm:@langchain/openai";
 import { ChatAnthropic } from "npm:@langchain/anthropic";
