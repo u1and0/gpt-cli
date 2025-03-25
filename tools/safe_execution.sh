35873d3e7d8ab617d962f2d342623f31d63e35e5
u1and0 <e01.ando60@gmail.com>
2025-03-25 14:33:15 +0900

    [refactor] パイプ入力を前提にコードを短くした

--- tools/safe_execution.sh
+++ tools/safe_execution.sh
@@ -6,29 +6,11 @@
 # usage:
 # $ echo "ls -l" | ./safe_execution.sh | bash
 
-# Check if input is from a pipe or arguments
-if [ -t 0 ]; then
-  # Input from arguments
-  code="$*"
-else
-  # Input from pipe
-  code=$(cat)
-fi
-
+code=$(cat)
 # Display the generated code
-# echo -e "\nGenerated code:"
 echo -e "Generated code:\n\n $code\n" >&2
-
-# Prompt for confirmation
-# read コマンドの入力を直接ターミナルから取得するために、
-# /dev/tty を使用します。
-read -n1 -p "Execute this code? (y/N): " yn < /dev/tty
-
 # Check the user's response and act accordingly
-if [[ $yn =~ [yY]  ]]; then
-    echo -e "\n$code" >&2
-    echo "$code"
-else
-    echo "Code execution aborted." >&2
-    exit 1
-fi
+read -n1 -p "Execute? (y/N): " yn < /dev/tty
+[[ $yn =~ [yY] ]] &&
+    { echo -e "\n$code" >&2; echo "$code"; } ||
+    { echo "Aborted." >&2; exit 1; }
