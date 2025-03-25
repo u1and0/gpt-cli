174b4ffd76f87205a6ac53add5e5d614263ec4e4
u1and0 <e01.ando60@gmail.com>
2025-03-25 14:16:37 +0900

    [feat] tools/safe_execution.sh コードの実行形式に抜き出してプロンプトで実行の許可を聞く

---
+++ tools/safe_execution.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# プロンプトの実行前にyes/no (y/N) を聞いてくる。
+# Yesの場合に実行する。
+# Noの場合はabortメッセージを出してエラーコード1で終了する。
+#
+# usage:
+# $ echo "ls -l" | ./safe_execution.sh | bash
+
+# Check if input is from a pipe or arguments
+if [ -t 0 ]; then
+  # Input from arguments
+  code="$*"
+else
+  # Input from pipe
+  code=$(cat)
+fi
+
+# Get code block surrounded by triple back quotes
+# and remove triple back quotes.
+code=$(echo "$code" | grep -ozP '```[\s\S]*?```' | sed '/^```/d;')
+
+# Display the generated code
+# echo -e "\nGenerated code:"
+echo -e "Generated code:\n\n $code\n" >&2
+
+# Prompt for confirmation
+# read コマンドの入力を直接ターミナルから取得するために、
+# /dev/tty を使用します。
+read -n1 -p "Execute this code? (y/N): " yn < /dev/tty
+
+# Check the user's response and act accordingly
+if [[ $yn =~ [yY]  ]]; then
+    echo -e "\n$code" >&2
+    echo "$code"
+else
+    echo "Code execution aborted." >&2
+    exit 1
+fi
